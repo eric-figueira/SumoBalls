3f619fc004159b4057d5a5ca72dc8fd147a5c551
marcoscotuquense <113925560+Marcos-Godinho-Filho@users.noreply.github.com>
2022-11-30 16:12:37 -0300

    Revert "."
    This reverts commit a16a087683646aae5f0a6031c73f4467a043ab2e.

--- Trabalho 3/TratadoraJogador.java
+++ Trabalho 3/TratadoraJogador.java
@@ -46,7 +46,7 @@ public class TratadoraJogador extends Thread
                 }
                 else if (this.servidor.espie() instanceof ComunicadoDeInicio)
                 {
-                    servidor.envie();
+                    ComunicadoDeInicio ci = (ComunicadoDeInicio) servidor.envie();
                     Cliente.iniciar();
                 }
             }
